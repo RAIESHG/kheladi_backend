93cc097cea8f5255083cfdd52cd8173ad65dece1
YugIot <yugiotinnovations@gmail.com>
2023-10-30 07:28:55 +0545

    ch

--- server.js
+++ server.js
@@ -2,106 +2,133 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const mqtt = require('mqtt');
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-require('dotenv').config();
+
+// Validations for required environment variables
 
 const THINGNAME = 'Yug'; // Your thing name
 const AWS_IOT_ENDPOINT =
     'a1lt7b88vgfoh5-ats.iot.ap-southeast-2.amazonaws.com'; // Your AWS IoT endpoint
 const topic = 'esp32/pub';
 
-// Amazon Root CA 1
-const rootCA = process.env.rootCA;
 
-// Device Certificate
-const cert = process.env.cert;
-
-// Device Private Key
-const privateKey = process.env.privateKey;
-
-// Socket.io Configuration
-// Socket.io Configuration
-const io = socketIo(server, {
-  cors: {
-      origin: "*",  // Allow all origins; in production, you should be more restrictive
-      methods: ["GET", "POST"]
+const requiredEnvVars = ['rootCA', 'cert', 'privateKey'];
+requiredEnvVars.forEach((varName) => {
+  if (!process.env[varName]) {
+    console.error(`Error: Missing required environment variable ${varName}`);
+    process.exit(1);
   }
 });
 
-// MQTT Configuration
 
+
+const rootCA = process.env.rootCA;
+const cert = process.env.cert;
+const privateKey = process.env.privateKey;
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",  // Allow all origins; in production, you should be more restrictive
+    methods: ["GET", "POST"]
+  }
+});
 
 const mqttOptions = {
-  key: Buffer.from(privateKey, 'utf8'), // Replace with your private key
-  cert: Buffer.from(cert, 'utf8'), // Replace with your certificate
-  ca: Buffer.from(rootCA, 'utf8'), // Replace with your root CA
-  clientId: 'myClientId',
-  protocol: 'mqtts', // Use 'mqtts' for secure MQTT
+  key: Buffer.from(privateKey, 'utf8'),
+  cert: Buffer.from(cert, 'utf8'),
+  ca: Buffer.from(rootCA, 'utf8'),
+  clientId: THINGNAME,
+  protocol: 'mqtts',
   host: AWS_IOT_ENDPOINT,
-  port: 8883, // Default port for MQTT over TLS
+  port: 8883,
 };
 
 console.log('Connecting to MQTT broker...');
-
-const client = mqtt.connect(mqttOptions);
+let client;
+try {
+  client = mqtt.connect(mqttOptions);
+} catch (error) {
+  console.error('Failed to connect to MQTT broker:', error);
+  process.exit(1);
+}
 
 let messages = [];
 
 client.on('connect', () => {
+  console.log('Connected to MQTT broker');
   try {
-    console.log('Connected to MQTT broker');
     client.subscribe(topic);
   } catch (error) {
     console.error('Error during MQTT connection:', error);
-    // Handle the error here, such as retrying the connection or taking other appropriate actions.
   }
 });
 
 client.on('message', (receivedTopic, payload) => {
-  const receivedMessage = payload.toString();
-  console.log(receivedMessage);
-  messages.push(receivedMessage);
-  io.emit('newMessage', receivedMessage);
+  try {
+    const receivedMessage = payload.toString();
+    console.log(receivedMessage);
+    messages.push(receivedMessage);
+    io.emit('newMessage', receivedMessage);
+  } catch (error) {
+    console.error('Error handling MQTT message:', error);
+  }
+});
+
+client.on('error', (error) => {
+  console.error('MQTT client error:', error);
+});
+
+client.on('close', () => {
+  console.log('MQTT connection closed');
 });
 
 io.on('connection', (socket) => {
   console.log('Client connected');
 
   socket.on('subscribeData', ({ topic }) => {
-    if (topic) {
-      // Unsubscribe from the previous topic, if any
-      client.unsubscribe(topic);
-    }
-
-    client.subscribe(topic, (error) => {
-      if (error) {
-        console.error('Error subscribing to topic:', error);
-      } else {
-        console.log(`Subscribed to topic: ${topic}`);
+    try {
+      if (topic) {
+        client.unsubscribe(topic);
       }
-    });
+      client.subscribe(topic, (error) => {
+        if (error) {
+          console.error('Error subscribing to topic:', error);
+        } else {
+          console.log(`Subscribed to topic: ${topic}`);
+        }
+      });
+    } catch (error) {
+      console.error('Error handling subscribeData event:', error);
+    }
   });
 
   socket.on('sendToBackend', ({ topic, message }) => {
-    // Handle messages sent to the backend (e.g., process them or perform actions)
-    console.log('Received message on the backend:', message);
+    try {
+      console.log('Received message on the backend:', message);
+    } catch (error) {
+      console.error('Error handling sendToBackend event:', error);
+    }
   });
 
   socket.on('sendToMQTT', ({ topic, message }) => {
-    if (!topic || !message) {
-      console.error('Both topic and message are required');
-      return;
-    }
-
-    client.publish(topic, message, (error) => {
-      if (error) {
-        console.error('Error publishing message to MQTT:', error);
-      } else {
-        console.log('Message published to MQTT successfully');
+    try {
+      if (!topic || !message) {
+        console.error('Both topic and message are required');
+        return;
       }
-    });
+      client.publish(topic, message, (error) => {
+        if (error) {
+          console.error('Error publishing message to MQTT:', error);
+        } else {
+          console.log('Message published to MQTT successfully');
+        }
+      });
+    } catch (error) {
+      console.error('Error handling sendToMQTT event:', error);
+    }
   });
 
   socket.on('disconnect', () => {
@@ -109,9 +136,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// ... Other API routes ...
-
-// Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
